1dcc89b80d8a94609efb0605f99a863ee3beb40e
pulaS29 <dev@pulane.co.za>
2018-02-14 16:48:24 +0200

    Added new files

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<div class="row-fluid">
+  <div id="footer" class="span12"> 2018 &copy; ODiTY QA Admin </div>
+</div>
+
+<!--end-Footer-part-->
+
+<script src="../js/jquery.min.js"></script> 
+<script src="../js/jquery.ui.custom.js"></script> 
+<script src="../js/bootstrap.min.js"></script> 
+<script src="../js/matrix.js"></script>
+</body>
+</html>
---
+++ includes/sidebar.php
@@ -0,0 +1,21 @@
+
+<!--sidebar-menu-->
+<div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
+  <ul>
+    <li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
+    <li> <a href="#"><i class="icon icon-signal"></i> <span>New Assessment</span></a> </li>
+    <li> <a href="#"><i class="icon icon-inbox"></i> <span>View QA'a</span></a> </li>
+    <li><a href="#"><i class="icon icon-th"></i> <span>Assessments</span></a></li>
+    <li><a href="#"><i class="icon icon-fullscreen"></i> <span>Assurances</span></a></li>
+    <li><a href="#"><i class="icon icon-tint"></i> <span>Completed</span></a></li>
+    <li><a href="#"><i class="icon icon-pencil"></i> <span>Variance</span></a></li>
+    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Agent Manager</span> <span class="label label-important">3</span></a>
+      <ul>
+        <li><a href="#">Add Agent</a></li>
+        <li><a href="#">Remove Agent</a></li>
+        <li><a href="#">Edit</a></li>
+      </ul>
+    </li>
+  </ul>
+</div>
+<!--sidebar-menu-->
